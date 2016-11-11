4a0b73565329d1ded96a27624f0f2afaf5485e65
bjornenalfa <bjorn.detterfelt@telia.com>
2016-11-11 20:41:27 +0100

    input checker

--- main.lua
+++ main.lua
@@ -13,7 +13,19 @@ end
 function love.draw()
   love.graphics.setColor(0,0,0)
   love.graphics.print(log, 0, 0)
-  if love.mouse.isDown() then
-    love.graphics.print("mouse down", 0, 20)
+  if love.mouse.isDown(1) then
+    love.graphics.print("mouse1 down", 0, 20)
   end
+  if love.mouse.isDown(2) then
+    love.graphics.print("mouse2 down", 0, 40)
+  end
+  if love.mouse.isDown(3) then
+    love.graphics.print("mouse3 down", 0, 60)
+  end
+  for i = 4, 20 do
+    if love.mouse.isDown(i) then
+      love.graphics.print("mouse"..i.." down", 0, 80)
+    end
+  end
+  
 end
