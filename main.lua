23fc0424e859f74f8fb031e9e948c344e024d9a9
bjornenalfa <bjorn.detterfelt@telia.com>
2016-11-11 20:26:22 +0100

    te

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
 
+log = ""
+
+function love.load()
+  love.graphics.setBackgroundColor(255,255,255)
+end
+
+
+function love.keypressed(key)
+  log = log.." "..key
+end
+
+function love.draw()
+  love.graphics.setColor(0,0,0)
+  love.graphics.print(log, 0, 0)
+  if love.mouse.isDown() then
+    love.graphics.print("mouse down")
+  end
+end
