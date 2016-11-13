5aa9fabed355de255beb1c6932d2e81fbaa8fd73
Johan Karlsson <johka036@student.liu.se>
2016-11-13 13:24:48 +0100

    Spawn fix

--- Game.lua
+++ Game.lua
@@ -45,23 +45,27 @@ end
 function Game.spawn(amount, forceSpawn)
   local monType
   local x, y
+  local side
   for i = 0, amount, 1 do
     if forceSpawn then
       monType = forceSpawn
     else
       monType = math.random(1, 3)
     end
-    x = math.random(0, 1)
-    y = math.random(0, 1)
-    if x then
+    -- 1=east, 2=west, 3=south, 4=north
+    side = math.random(1, 4)
+    if side == 1 then
       x = Map.width + 50
-    else
+      y = math.random(0, Map.height)
+    elseif side == 2 then
       x = -50
-    end
-    if y then
+      y = math.random(0, Map.height)
+    elseif side == 3 then
       y = Map.height + 50
+      x = math.random(0, Map.width)
     else
       y = -50
+      x = math.random(0, Map.width)
     end
     if monType == 1 then
       Zombieduck.new(x, y)
@@ -78,7 +82,7 @@ end
 
 function Game.update(dt)
   Game.timer = Game.timer + dt
-  if Game.timer > 1  and #Monster.list < 30 then
+  if Game.timer > 2  and #Monster.list < 30 then
     if math.random(1, 10) == 1 then
       Game.spawn(1, 4) -- duckcrab
     end
