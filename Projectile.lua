5ae4fa996e8da24182f407b5b0b1d75b1292ed08
bjornenalfa <bjorn.detterfelt@telia.com>
2016-11-13 13:30:00 +0100

    another print

--- Projectile.lua
+++ Projectile.lua
@@ -16,8 +16,6 @@ function Projectile.new(x, y, angle, damage, speed)
   setmetatable(new, Projectile)
   table.insert(Projectile.list, new)
   
-  --if new.vx == 0 then print(new.vy) end
-  
   return new
 end
 
